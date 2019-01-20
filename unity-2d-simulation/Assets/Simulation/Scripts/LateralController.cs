cfedf77e151f5d62872b3c573237c34c28b03bce
Matthew Dias <matthew.d.dias@gmail.com>
2019-01-20 13:51:48 -0500

    Refactor lab PID and lateral controller code (missing implementation for getYaw() function)

---
+++ unity-2d-simulation/Assets/Simulation/Scripts/LateralController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateralController {
+
+    protected bool goalReceived;
+    protected Pose goal;
+
+    public LateralController()
+    {
+        goalReceived = false;
+    }
+
+    public void setGoal(Pose g)
+    {
+        goalReceived = true;
+        goal = g;
+    }
+
+    public Pose getGoal()
+    {
+        return goal;
+    }
+
+    protected Vector3 findIntersection(Vector3 point, Vector3 linePoint, float lineAngle)
+    {
+        return findIntersection(point, linePoint, new Vector3(1, Mathf.Tan(lineAngle), 0));
+    }
+
+    protected Vector3 findIntersection(Vector3 point, Vector3 linePoint, Vector3 lineSlope)
+    {
+        Vector3 pointSlope = new Vector3(lineSlope.y, -lineSlope.x, 0);
+
+        /*
+         * Two parameteric equations:
+         * L0 = lineSlope * t + linePoint
+         * L1 = pointSlope * u + point
+         * Solve for t.
+         */
+        float intersectT = (pointSlope.y * (point.x - linePoint.x) -
+                            pointSlope.x * (point.y - linePoint.y)) /
+                            (lineSlope.x * pointSlope.y - pointSlope.x * lineSlope.y);
+
+        return lineSlope * intersectT + linePoint;
+    }
+
+    //TODO - implement this function
+    protected float getYaw(Quaternion rotation)
+    {
+        return 0f;
+    }
+}
---
+++ unity-2d-simulation/Assets/Simulation/Scripts/PIDController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIDController {
+
+    public float p;
+    public float i;
+    public float d;
+    private float saturationMin;
+    private float saturationMax;
+    private bool prevValid = false;
+    private bool goalValid = false;
+    private bool accumulate = true;
+    private float goal;
+    private float accum;
+    private float prevError;
+
+    public PIDController(float p, float d, float i, float saturationMin, float saturationMax)
+    {
+        this.p = p;
+        this.i = i;
+        this.d = d;
+        this.saturationMin = saturationMin;
+        this.saturationMax = saturationMax;
+        prevValid = false;
+        goalValid = false;
+        accumulate = true;
+        accum = 0f;
+
+        checkSaturation();
+    }
+
+    public void setGoal(float r)
+    {
+        goalValid = true;
+        goal = r;
+    }
+
+    public void setSaturation(float min, float max)
+    {
+        saturationMin = min;
+        saturationMax = max;
+
+        // Inherit the accumulation status, but trim the accum value
+        accum = Mathf.Clamp(accum, saturationMin, saturationMax);
+
+        checkSaturation();
+    }
+
+    private void checkSaturation()
+    {
+        if (saturationMin > saturationMax) {
+            throw new System.ArgumentException("Saturation min value must be smaller than the max value.");
+        }
+    }
+
+    public float commandStep(float feedforward, float y, float dt, bool doAccumulate)
+    {
+        if (!goalValid) {
+            return 0f;
+        }
+
+        float e = goal - y;
+
+        float pTerm = p * e;
+        float totalTerm = pTerm + feedforward;
+
+        if (!prevValid) {
+            prevError = e;
+            prevValid = true;
+            return Mathf.Clamp(totalTerm, saturationMin, saturationMax);
+        }
+
+        // Accumulate if told by user AND windup prevention not in place
+        if (doAccumulate && accumulate) {
+            // Trapezoidal integration
+            accum += (prevError + e) / 2 * dt;
+        }
+
+        float iTerm = i * accum;
+        float dTerm = d * (e - prevError) / dt;
+
+        totalTerm += iTerm + dTerm;
+
+        // Update prev
+        prevError = e;
+
+        // Update windup prevention
+        bool saturating = totalTerm < saturationMin || totalTerm > saturationMax;
+        bool errorAccumSignMatch = accum * prevError > 0;
+        accumulate = !(saturating && errorAccumSignMatch);
+
+        return Mathf.Clamp(totalTerm, saturationMin, saturationMax);
+    }
+
+    public void reset()
+    {
+        accum = 0f;
+        prevValid = false;
+        accumulate = true;
+    }
+}
