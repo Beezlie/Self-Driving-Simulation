671e703f60bc1fa380f46bc19b7fcf27bf350f51
Samuel Yigzaw <syigzaw@uwaterloo.ca>
2019-03-06 13:45:56 -0500

    Merging ML-Agents changes into the rest of the project

--- RC-Car-Self-Driving/Assets/Scripts/Car/EgoCarInterface.cs
+++ RC-Car-Self-Driving/Assets/Scripts/Car/EgoCarInterface.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAgents;
+using System.Linq;
 
-public class EgoCarInterface : MonoBehaviour {
+public class EgoCarInterface : Agent {
 
     // Car Controls
     private CarController carController;
@@ -158,5 +160,133 @@ public class EgoCarInterface : MonoBehaviour {
 
             angle += (360f / numLineSegments);
         }
+    }
+
+//////////////////////////////////////////////////
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTargetPosition(target);
+        // AddReward(-0.1f);
+        // resetCount = resetCount + 1;
+        // if ((transform.position - GetTargetPosition()).sqrMagnitude < 5) {
+        //     resetCount = 0;
+        // }
+        // Debug.Log(resetCount);
+            // RequestDecision();
+        // } else if (resetCount > 20) { // CHANGE THIS!!!
+        //     Debug.Log("Running In Circles");
+        //     resetCount = 0;
+        //     RequestDecision();
+        // }
+        if (transform.position.x >= mapLength - 10) {
+            Debug.Log("Finished");
+            // resetCount = 0;
+            SetReward(1.0f);
+            finished = true;
+            Done();
+        } else if (
+            GetTargetPosition().x < 10 || 
+            GetTargetPosition().z < 10 ||
+            GetTargetPosition().z > 40) {
+            Debug.Log("Out Of Bounds");
+            // resetCount = 0;
+            SetReward(-1.0f);
+            finished = false;
+            Done();
+        }
+    }
+
+    public override void AgentReset()
+    {
+        agentResetCount = agentResetCount + 1;
+        Debug.Log(string.Format("Agent Reset: {0}", agentResetCount));
+        Pathfinding.GetComponent<ObstaclesController>().InitObstacles();
+        transform.position = new Vector3(startingXPos, 0, 25);
+        transform.eulerAngles = new Vector3(0, 90, 0);
+
+        //Reset target position
+        targetPosition = transform.position;
+        Debug.Log(transform.position);
+        if (finished) {
+            wins = wins + 1;
+        } else if (transform.position.x > 30) {
+            loses = loses + 1;
+        }
+        Debug.Log(wins);
+        Debug.Log(loses);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected");
+        SetReward(-1.0f);
+        finished = false;
+        Done();
+    }
+
+    //Search through all obstacles to find which fall within a radius of the car
+    public List<ObstacleData> DetectNClosestObstacles(int N)
+    {
+        //The list with close obstacles
+        List<ObstacleData> closeObstacles = new List<ObstacleData>();
+
+        //The list with all obstacles in the map
+        List<ObstacleData> allObstacles = ObstaclesController.obstaclesPosList;
+
+        //Find close obstacles
+        for (int i = 0; i < allObstacles.Count; i++)
+        {
+            closeObstacles.Add(allObstacles[i]);
+        }
+        List<ObstacleData> firstNClosestObstacles = closeObstacles
+            .OrderBy(obs => (transform.position - obs.centerPos).magnitude)
+            .Take(N)
+            .ToList();
+
+        return firstNClosestObstacles;
+    }
+
+    public override void CollectObservations() {
+        Debug.Log("Collecting Observations");
+        AddVectorObs(transform.position.x/mapLength);
+        AddVectorObs(transform.position.z/mapWidth);
+
+        int numOfObstaclesDetected = 5;
+        List<ObstacleData> obstacleList = DetectNClosestObstacles(numOfObstaclesDetected);
+        for (int i = 0; i < obstacleList.Count; i++) {
+            AddVectorObs((obstacleList[i].centerPos.x - transform.position.x)/mapLength);
+            AddVectorObs((obstacleList[i].centerPos.z - transform.position.z)/mapWidth);
+            AddVectorObs((obstacleList[i].cornerPos.BL.x - transform.position.x)/mapLength);
+            AddVectorObs((obstacleList[i].cornerPos.FR.x - transform.position.x)/mapLength);
+            AddVectorObs((obstacleList[i].cornerPos.BL.z - transform.position.z)/mapWidth);
+            AddVectorObs((obstacleList[i].cornerPos.FR.z - transform.position.z)/mapWidth);
+        }
+        if (numOfObstaclesDetected > obstacleList.Count) {
+            for (int i = 0; i < numOfObstaclesDetected - obstacleList.Count; i++) {
+                AddVectorObs(0);
+                AddVectorObs(0);
+            }
+        }
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction) {
+        if (vectorAction[0] != oldActionX || vectorAction[1] != oldActionZ) {
+            Debug.Log("Acting");
+            float norm = Mathf.Sqrt(Mathf.Pow(vectorAction[0], 2) + Mathf.Pow(vectorAction[1], 2));
+            target = new Vector3(
+                Mathf.Clamp(transform.position.x + vectorAction[0]*20/norm, 0, mapLength), 
+                0, 
+                transform.position.z + vectorAction[1]*20/norm);
+            // SetTargetPosition(waypoint);
+            oldActionX = vectorAction[0];
+            oldActionZ = vectorAction[1];
+        }
+        SetReward(transform.position.x - currentXPos);
+        // Debug.Log(string.Format("Reward: {0}", transform.position.x - currentXPos));
+        currentXPos = transform.position.x;
+        // Debug.Log(transform.position);
+        // Debug.Log(GetTargetPosition());
     }
 }
