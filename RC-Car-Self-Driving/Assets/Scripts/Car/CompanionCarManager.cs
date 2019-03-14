8ba9be83536529ee7a314981586cb418cfe2370f
Matthew Dias <matthew.d.dias@gmail.com>
2019-03-14 17:14:30 -0400

    created separate update functions for pursuit and normal goal setting so they can be called at different rates

--- RC-Car-Self-Driving/Assets/Scripts/Car/CompanionCarManager.cs
+++ RC-Car-Self-Driving/Assets/Scripts/Car/CompanionCarManager.cs
@@ -20,6 +20,10 @@ public class CompanionCarManager : MonoBehaviour {
     private float laneMidPointOffset;
     private float[] laneLines;
 
+    // Driving mode goal updates
+    private float normalModeGoalUpdateRate = 10f;
+    private float pursuitModeGoalUpdateRate = 3f;
+
     public void SetDrivingMode(string carName, DrivingMode mode)
     {
         companionCarModes[carName] = mode;
@@ -66,11 +70,13 @@ public class CompanionCarManager : MonoBehaviour {
             }
         }
 
-        // Update companion car goal position every 10 seconds
-        InvokeRepeating("UpdateGoal", 0f, 10f);
+        // Update normal mode companion car goal position every 10 seconds
+        InvokeRepeating("UpdateNormalDrivingGoal", 0f, normalModeGoalUpdateRate);
+        // Update pursuit mode companion car goal position every 3 seconds
+        InvokeRepeating("UpdatePursuitDrivingGoal", 0f, pursuitModeGoalUpdateRate);
     }
 
-    private void UpdateGoal()
+    private void UpdatePursuitDrivingGoal()
     {
         foreach (KeyValuePair<GameObject, int> entry in companionCars)
         {
@@ -78,42 +84,46 @@ public class CompanionCarManager : MonoBehaviour {
             int lane = entry.Value;
             DrivingMode drivingMode = companionCarModes[car.name];
 
-            // Select lane and longitudinal goal
-            int maxLaneChange = 1;
-            float maxZ = trackLength / 2;
-
-            float goalZ;
-            int newLane;
-            Debug.Log(string.Format("Mode, {0}", drivingMode));
-            switch(drivingMode)
+            if (drivingMode == DrivingMode.PURSUIT)
             {
-                default:
-                {
-                    goalZ = Mathf.Clamp(Random.Range(car.transform.position.z - maxZ, car.transform.position.z + maxZ), trackLength * 0.1f, trackLength * 0.9f);
-                    newLane = Mathf.Clamp(Random.Range(lane - maxLaneChange, lane + maxLaneChange), 0, numLanes);
-                    break;
-                }
-                case DrivingMode.PURSUIT:
-                {
-                    goalZ = egoCar.transform.position.z;
-                    newLane = Mathf.Clamp(NearestLane(egoCar.transform.position.x), lane - maxLaneChange, lane + maxLaneChange);
-                    break;
-                }
+                float goalZ = egoCar.transform.position.z;
+                float goalX = egoCar.transform.position.x;
+                car.gameObject.GetComponent<CompanionCarInterface>().SetTargetPosition(new Vector3(goalX, 0, goalZ));
             }
+        }
+    }
 
-            // Only change lanes if not moving backwards
-            if (goalZ < car.transform.position.z)
+    private void UpdateNormalDrivingGoal()
+    {
+        foreach (KeyValuePair<GameObject, int> entry in companionCars)
+        {
+            GameObject car = entry.Key;
+            int lane = entry.Value;
+            DrivingMode drivingMode = companionCarModes[car.name];
+
+            if (drivingMode == DrivingMode.NORMAL)
             {
-                newLane = lane;
-            } 
+                // Select lane and longitudinal goal
+                int maxLaneChange = 1;
+                float maxZ = trackLength / 2;
 
-            Vector3 goalPos = new Vector3(laneLines[newLane] - laneMidPointOffset, 0, goalZ);
+                float goalZ = Mathf.Clamp(Random.Range(car.transform.position.z - maxZ, car.transform.position.z + maxZ), trackLength * 0.1f, trackLength * 0.9f);
+                int newLane = Mathf.Clamp(Random.Range(lane - maxLaneChange, lane + maxLaneChange), 0, numLanes);
 
-            // if (!PathOccupied(car, goalPos))
-            // {
+                // Only change lanes if not moving backwards
+                if (goalZ < car.transform.position.z)
+                {
+                    newLane = lane;
+                }
+
+                Vector3 goalPos = new Vector3(laneLines[newLane] - laneMidPointOffset, 0, goalZ);
+
+                // if (!PathOccupied(car, goalPos))
+                // {
                 Debug.Log(string.Format("{0} goal: {1}", car.name, goalPos));
                 car.gameObject.GetComponent<CompanionCarInterface>().SetTargetPosition(goalPos);
-            // }
+                // }
+            }
         }
     }
 
